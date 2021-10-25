0b4859b7841ce1dcb3838327ce226b1d59a3669a
Pedro <pfbaltariead@gmail.com>
2021-10-25 00:08:12 -0300

    Added/configured GitHub OAuth

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
 
-app.listen(4000, () => console.log(`Server is running on PORT:4000`));
+app
+  .get("/github", (req, res) => {
+    res.redirect(
+      `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+    );
+  })
+  .listen(4000, () => console.log(`Server is running on PORT:4000`));
