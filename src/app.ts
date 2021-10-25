e2f127cd14c0773518f3aad4c089a4c44ee34a23
Pedro <pfbaltariead@gmail.com>
2021-10-25 00:12:06 -0300

    Created callback route

--- src/app.ts
+++ src/app.ts
@@ -9,4 +9,9 @@ app
       `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
     );
   })
+  .get("/singin/callback", (req, res) => {
+    const { code } = req.query;
+
+    return res.json(code);
+  })
   .listen(4000, () => console.log(`Server is running on PORT:4000`));
